3b3e6ee776b132dee9cc9574dbed75c43e21af2d
Xi Kong <93821098+XiKong31@users.noreply.github.com>
2023-05-16 17:24:21 +0800

    Update GridPaneExample.java
    rewrite

--- ataxx/demo/GridPaneExample.java
+++ ataxx/demo/GridPaneExample.java
@@ -6,11 +6,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -18,17 +14,47 @@ import javafx.stage.Stage;
 public class GridPaneExample extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane root = new BorderPane();
-        setMenu(root);
-        setBoard(root);
+        BorderPane root = createRootPane();
         Scene scene = new Scene(root, 800, 800);
         primaryStage.setScene(scene);
         primaryStage.setTitle("GridPane Example");
         primaryStage.show();
     }
 
+    private BorderPane createRootPane() {
+        BorderPane root = new BorderPane();
+        root.setTop(createMenuBar());
+        root.setCenter(createGridPane());
+        return root;
+    }
 
-    public void setBoard(BorderPane root){
+    private MenuBar createMenuBar() {
+        // Create menu items
+        MenuItem newItem = new MenuItem("New");
+        MenuItem openItem = new MenuItem("Open");
+        MenuItem saveItem = new MenuItem("Save");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        // Create game menu and add menu items
+        Menu gameMenu = new Menu("Game");
+        gameMenu.getItems().addAll(newItem, openItem, saveItem, exitItem);
+
+        MenuItem cutItem = new MenuItem("Cut");
+        MenuItem copyItem = new MenuItem("Copy");
+        MenuItem pasteItem = new MenuItem("Paste");
+
+        // Create edit menu and add menu items
+        Menu editMenu = new Menu("Edit");
+        editMenu.getItems().addAll(cutItem, copyItem, pasteItem);
+
+        // Create menu bar and add menus
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(gameMenu, editMenu);
+
+        return menuBar;
+    }
+
+    private GridPane createGridPane() {
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10));
         grid.setHgap(0);
@@ -37,73 +63,56 @@ public class GridPaneExample extends Application {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (i == 0 && j != 7) {
-                    Label label = new Label(7 - j + "");
+                    Label label = createLabel(7 - j);
                     GridPane.setConstraints(label, i, j);
                     grid.getChildren().add(label);
-                    label.setPrefSize(40, 40);
-                    label.setAlignment(Pos.CENTER);
-                    GridPane.setHalignment(label, HPos.LEFT);
-                    GridPane.setValignment(label, VPos.CENTER);
-//                    SStyleHelper.node(label).addStyle("-fx-border-color", "#a7bfd8").apply();
                     continue;
                 }
                 if (j == 7 && i != 0) {
-                    int ascii = 64 + i;
-                    char ch1 = (char) ascii;
-                    Label label = new Label(ch1 + "");
-                    label.setPrefSize(40, 40);
-                    label.setAlignment(Pos.CENTER);
+                    char ch1 = (char) (64 + i);
+                    Label label = createLabel(ch1);
                     GridPane.setConstraints(label, i, j);
                     grid.getChildren().add(label);
-                    GridPane.setHalignment(label, javafx.geometry.HPos.CENTER);
-                    GridPane.setValignment(label, VPos.BOTTOM);
-//                    SStyleHelper.node(label).addStyle("-fx-border-color", "#a7bfd8").apply();
                     continue;
                 }
                 if (i == 0 && j == 7) {
                     continue;
                 }
-                Button button = new Button("");
-                button.setPrefSize(40, 40);
+                Button button = createButton();
                 GridPane.setConstraints(button, i, j);
                 grid.getChildren().add(button);
             }
         }
-        SStyleHelper.node(grid).addStyle("-fx-border-color", "#a7bfd8").apply();
-        root.setCenter(grid);
+
+        grid.setStyle("-fx-border-color: #a7bfd8;");
+        return grid;
     }
 
+    private Label createLabel(Object value) {
+        Label label = new Label(value.toString());
+        label.setPrefSize(40, 40);
+        label.setAlignment(Pos.CENTER);
+        if (value instanceof Integer) {
+            GridPane.setHalignment(label, HPos.LEFT);
+            GridPane.setValignment(label, VPos.CENTER);
+        } else if (value instanceof Character) {
+            GridPane.setHalignment(label, HPos.CENTER);
+            GridPane.setValignment(label, VPos.BOTTOM);
+        }
+        return label;
+    }
 
-
-
-    public void setMenu(BorderPane root){
-        // 创建菜单项
-        MenuItem newItem = new MenuItem("New");
-        MenuItem openItem = new MenuItem("Open");
-        MenuItem saveItem = new MenuItem("Save");
-        MenuItem exitItem = new MenuItem("Exit");
-
-        // 创建菜单并添加菜单项
-        Menu gameMenu = new Menu("Game");
-        gameMenu.getItems().addAll(newItem, openItem, saveItem, exitItem);
-
-        MenuItem cutItem = new MenuItem("Cut");
-        MenuItem copyItem = new MenuItem("Copy");
-        MenuItem pasteItem = new MenuItem("Paste");
-
-        // 创建菜单并添加菜单项
-        Menu editMenu = new Menu("Edit");
-        editMenu.getItems().addAll(cutItem, copyItem, pasteItem);
-
-        // 创建菜单栏并添加菜单
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(gameMenu, editMenu);
-
-        // 将菜单栏添加到布局中
-        root.setTop(menuBar);
+    private Button createButton() {
+        Button button = new Button("");
+        button.setPrefSize(40, 40);
+        return button;
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
+
 }
